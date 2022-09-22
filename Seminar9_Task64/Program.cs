7c6755c0075771bde5e9ce242cd533782326e1ec
Galina <pgv21@yandex.ru>
2022-09-22 16:52:50 +0300

    save1

---
+++ Seminar9_Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+int M = 1;
+int N = 5;
+
+string NaturalNum (int M, int N)
+{
+    if (M<=N) return $"{M}" + NaturalNum(M+1, N);
+    else return string.Empty;
+}
+
+Console.WriteLine(NaturalNum(M, N));
